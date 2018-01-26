b4404cff5dbe5a8fb2a041e1088000a1226b158f
Pianomen <35760191+Pianomen@users.noreply.github.com>
2018-01-26 22:22:08 +0400

    continuing code
    continue code...

--- registrate.php
+++ registrate.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php ini_set('display_errors', '1');
+error_reporting(E_ALL); ?>
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
  <link rel="stylesheet" href="style.css">
  <title>Registration</title>
-
+<? var_dump($_REQUEST); ?>
 <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
- <form class="frmm" action="validate.php" method="POST" enctype="multiparte/formdata">
-   <label><span>Name:</span> <br><input type="text" class="user" name="user" placeholder="Name"></label><br>
-   <label><span>Surname:</span> <br><input type="text" class="surname" name="surname" placeholder="Surname"></label><br>
-   <label><span>Email:</span> <br><input type="email" class="email" name="email" placeholder="Email"></label><br>
-   <label><span>Username:</span> <br><input type="text" class="username" name="username" placeholder="Username"></label><br>
-   <label><span>Password:</span> <br><input type="password" class="password" name="pswd" placeholder="Passord"></label><br>
-   <label><span>Repeat password:</span> <br><input type="text" class="repeat" name="reppswd" placeholder="Prepeat password"></label><br>
-   <label>
+ <form class="form" action="validate.php" method="POST" enctype="multiparte/formdata">
+   <label><span class="title">Name:</span> <br><input type="text" class="user" name="user" value="" placeholder="Name"></label><span class="warning"></span><br>
+   <label><span class="title">Surname:</span> <br><input type="text" class="surname" name="surname" value="" placeholder="Surname"></label><span class="warning"></span><br>
+   <label><span class="title">Email:</span> <br><input type="email" class="email" name="email" value="" placeholder="Email"></label><br>
+   <span class="warning"></span><label><span class="title">Username:</span><br><input type="text" class="username" name="username" placeholder="Username"></label><span class="warning"></span><br>
+   <label><span class="title">Password:</span> <br><input type="password" class="password" name="pswd" placeholder="Passord"></label><span class="warning"></span><br>
+   <label><span class="title">Repeat password:</span> <br><input type="password" class="repeat" name="reppswd" placeholder="Prepeat password"></label><span class="warning"></span><br>
+   <label><br>
    <span>Gender:</span><br>
-   <sapn>Male</sapn><input type="radio" name="gender" calss="male">
-   <span>Famale</span><input type="radio" name="gender" calss="famale">
-   </label><br>
+   <sapn>Male</sapn><input type="radio" name="gender" calss="male" value="Male">
+   <span>Famale</span><input type="radio" name="gender" calss="famale" value="Famale">
+   </label><br><br>
    <label class='capchalbl'><span>Recapcha:</span><div class="g-recaptcha" data-sitekey="6LfZC0IUAAAAAK55XXxLebUwg6LZcsCYp7zOQWpM"></div></label><br>
    <label><input type="submit" class="send" value="Send" name="send"></label>
-
+ <center>
+ <?php var_dump($_POST) ?>
+ </center>
  </form>
+
  <script tyle="text/javascript" src="validate.js"></script>
 </body>
 </html>
