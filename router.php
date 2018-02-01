ca6de534890a862f3780cc00e8ecf60799838668
Mainer <webdevericm@gmail.com>
2018-02-01 17:16:47 +0400

    new

---
+++ router.php
@@ -0,0 +1,23 @@
+<?php
+//Security issue
+require_once(__DIR__.'/load.php');
+if(!defined(LOAD)){
+	exit('Fuck You');
+}
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+require_once(__DIR__.'/classes.php');
+
+//ini_set('display_errors', E_ALL);
+/**
+* 	Class for connect a Datebase an get a data
+*/
+
+
+/**
+* 	Class get a user data in form and
+*/
+
+$usr1 = new User(); // $userdata set atribute
+$usr1->create();
+$usr1->login_user($_POST['email'], $_POST['password']);
